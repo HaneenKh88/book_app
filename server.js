8b61693149ffe37a58fc1da840c255ed99d67132
Haneen <haneen.khasawneh96@gmail.com>
2021-03-02 04:48:05 +0200

    lab12 complete

--- server.js
+++ server.js
@@ -1,48 +1,72 @@
 'use strict'
 
-const express =  require('express');
-require('dotenv').config;
-const superagent = require('superagent');
-
-const PORT = process.env.PORT || 3000;
+const express = require('express')
 const app = express();
 
-app.use(express.static('./public'));
-app.use(express.urlencoded({extended:true}));
+require('dotenv').config();
 
-app.set('view engine','ejs');
+const cors = require('cors');
+app.use(cors());
+
+
+const pg = require('pg')
+const client = new pg.Client(process.env.DATABASE_URL);
+
+
+const superagent = require('superagent');
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 
 
 //Routes Defenition
+app.get('/' , AllBooksHandler);
+app.get('/searches/new', searchResultHandler);
+app.post('/searches' ,searchHandler);
+app.post('/addbooks' , AddBooksHandler);
+app.get('/books/:id' , BooksDetailHandler);
 
-// app.get('/search', searchHandler);
+function AllBooksHandler (req,res){
+  let SQL = `SELECT * FROM books;`
 
+  client.query(SQL) 
+  .then (result =>
+    {
+      res.render('pages/index' , {booklist: result.rows});
+      console.log(result.rows);
+    })
+    .catch(() => {
+      errorHandler(`Error`, req, res);
+    })
+};
 
-app.get('/',(req,res)=>{
-    // res.send('hello');
-    res.render('pages/index');
-})
-
-app.get('/searches/new',(req,res)=>{
+function searchResultHandler (req,res){
     res.render('pages/searches/new');
-   })
+   }
 
 
-app.post('/searches',(req,res) => 
+   function searchHandler(req,res) 
 {
     let BookSelector = req.body.SearchBox;
-    
-    let Result;
+
+    let url;
+
 
          if(req.query.select === 'Title')
-         Result = 'intitle';
-    
-         else(req.query.select === 'Author')
-         Result = 'Author';
-        
-         let url = `https://www.googleapis.com/books/v1/volumes?q=${BookSelector}+${Result}:keyes`;
-    
+         {
+         url = `https://www.googleapis.com/books/v1/volumes?q=${BookSelector}+intitle`;
+         }
 
+         else(req.query.select === 'Author')
+       {
+         url =`https://www.googleapis.com/books/v1/volumes?q=${BookSelector}+inauthor`;
+       }
+
+       
     superagent.get(url)
          .then (BooksInfo =>
             {
@@ -53,9 +77,46 @@ app.post('/searches',(req,res) =>
                     })
                     res.render('pages/searches/show', {BooksList : BooksArr});
             })
-})
+}
 
-   
+function AddBooksHandler(req,res)
+{
+  let SQL = ` INSERT INTO books(title,author,isbn,image_url,description) VALUES ($1, $2, $3, $4, $5) RETURNING id ;`
+
+  let Body = req.body;
+  let values = [Body.title, Body.author, Body.isbn, Body.image_url, Body.description];
+
+
+  client.query(SQL , values) 
+  .then (() =>
+    {
+      res.redirect('/');
+    })
+    .catch(() => {
+      errorHandler(`Error`, req, res);
+    })
+}
+
+function BooksDetailHandler(req,res)
+{
+  let SQL = `SELECT * FROM books WHERE id = $1;`;
+  let values= [req.params.id];
+
+  client.query(SQL ,values)
+  .then((result) =>
+  {
+    res.render('pages/books/detail' , {booklist: result.rows[0]});
+  })
+}
+
+
+function errorHandler(errors) {
+  app.use("*", (req, res) => {
+    res.status(500).send(errors);
+  })
+}
+
+
 // Books Constructor
 
 function Books(BooksProerty)
@@ -64,8 +125,14 @@ function Books(BooksProerty)
   this.author = BooksProerty.volumeInfo.authors;
   this.description = BooksProerty.volumeInfo.description;
   this.imgUrl = BooksProerty.volumeInfo.imageLinks.thumbnail;
+  this.isbn = BooksProerty.volumeInfo.industryIdentifiers[0].type + BooksProerty.volumeInfo.industryIdentifiers[0].identifier
 }
 
-app.listen(PORT,()=>{
-    console.log(`Listening on PORT ${PORT}`);
+// app.listen(PORT,()=>{
+//     console.log(`Listening on PORT ${PORT}`);
+// })
+
+client.connect()
+.then(() => {
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 })
